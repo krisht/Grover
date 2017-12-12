c23b3da0030fbf51f67d02d807124a46e4555df0
Abhinav Jain <jain@cooper.edu>
2017-12-12 15:14:05 -0500

    updated from usb drive

--- detector.py
+++ detector.py
@@ -6,58 +6,60 @@ import shutil
 
 
 def find_biggest_contour(image):
-	# Copy to prevent modification
-	image = image.copy()
-	_, contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # Copy to prevent modification
+    image = image.copy()
+    _, contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
+    centers = []
+    for ii in range(len(contours)):
+        moments = cv2.moments(contours[ii])
+        centers.append((int(moments['m10'] / moments['m00']), int(moments['m01'] / moments['m00'])))
 
-	centers = []
-	for ii in range(len(contours)):
-		moments = cv2.moments(contours[ii])
-		centers.append((int(moments['m10'] / moments['m00']), int(moments['m01'] / moments['m00'])))
+    mask = np.zeros(image.shape, np.uint8)
+    cv2.drawContours(mask, contours, -1, 255, -1)
+    return contours, mask, centers
 
-	mask = np.zeros(image.shape, np.uint8)
-	cv2.drawContours(mask, contours, -1, 255, -1)
-	return contours, mask, centers
 
 if __name__ == '__main__':
 
-	files = [os.path.join('./inputs', file) for file in os.listdir('./inputs') if file.endswith('.jpeg') or file.endswith('.jpg')]
+    files = [os.path.join('./inputs', f) for f in os.listdir('./inputs') if f.endswith('.jpeg') or f.endswith('.jpg')]
 
-	if os.path.exists("./outputs"):
-		shutil.rmtree('./outputs')
-	os.makedirs("./outputs")
+    if os.path.exists("./outputs"):
+        shutil.rmtree('./outputs')
+    os.makedirs("./outputs")
 
-	for fname in files:
-		# Convert image to RGB
-		image = cv2.imread(fname)
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-		# Blur image slightly
-		image_blur = cv2.GaussianBlur(image, (7, 7), 0)
-		image_blur_hsv = cv2.cvtColor(image_blur, cv2.COLOR_RGB2HSV)
-		# 0-10 hue
-		min_red = np.array([0, 100, 80])
-		max_red = np.array([10, 256, 256])
-		image_red1 = cv2.inRange(image_blur_hsv, min_red, max_red)
-		# 170-180 hue
-		min_red2 = np.array([170, 100, 80])
-		max_red2 = np.array([180, 256, 256])
-		image_red2 = cv2.inRange(image_blur_hsv, min_red2, max_red2)
-		image_red = image_red1 + image_red2
-		kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
-		image_red_closed = cv2.morphologyEx(image_red, cv2.MORPH_CLOSE, kernel)
-		image_red_closed_then_opened = cv2.morphologyEx(image_red_closed, cv2.MORPH_OPEN, kernel)
+    for f in files:
+        # Convert image to RGB from BGR
+        image = cv2.imread(f)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-		contours, red_mask, centers = find_biggest_contour(image_red_closed_then_opened)
+        # Blur image slightly
+        image_blur = cv2.GaussianBlur(image, (7, 7), 0)
+        image_blur_hsv = cv2.cvtColor(image_blur, cv2.COLOR_RGB2HSV)
 
-		image_with_com = image.copy()
-		for center in centers:
-			cv2.circle(image_with_com, tuple(center), 10, (255, 255, 0), -1)
-		plt.imsave("./outputs/" + os.path.basename(fname).split('.')[0] + "_com.png", image_with_com)
+        # 0-10 hue
+        min_red = np.array([0, 100, 80])
+        max_red = np.array([10, 256, 256])
+        image_red1 = cv2.inRange(image_blur_hsv, min_red, max_red)
+        # 170-180 hue
+        min_red2 = np.array([170, 100, 80])
+        max_red2 = np.array([175, 256, 256])
+        image_red2 = cv2.inRange(image_blur_hsv, min_red2, max_red2)
+        image_red = image_red1 + image_red2
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+        image_red_closed = cv2.morphologyEx(image_red, cv2.MORPH_CLOSE, kernel)
+        image_red_closed_then_opened = cv2.morphologyEx(image_red_closed, cv2.MORPH_OPEN, kernel)
 
-		image_with_ellipse = image.copy()
+        contours, red_mask, centers = find_biggest_contour(image_red_closed_then_opened)
 
-		for contour in contours:
-			ellipse = cv2.fitEllipse(contour)
-			cv2.ellipse(image_with_ellipse, ellipse, (255, 255, 0), 5)
-		plt.imsave("./outputs/" + os.path.basename(fname).split('.')[0] + "_ellipse.png", image_with_ellipse)
+        image_with_com = image.copy()
+        for center in centers:
+            cv2.circle(image_with_com, tuple(center), 10, (255, 255, 0), -1)
+        plt.imsave("./outputs/" + os.path.basename(f).split('.')[0] + "_com.png", image_with_com)
+
+        image_with_ellipse = image.copy()
+
+        for contour in contours:
+            ellipse = cv2.fitEllipse(contour)
+            cv2.ellipse(image_with_ellipse, ellipse, (255, 255, 0), 5)
+        plt.imsave("./outputs/" + os.path.basename(f).split('.')[0] + "_ellipse.png", image_with_ellipse)

a815ffa19be9db329acb216496a3424e1ec194c1
Krishna Thiyagarajan <krisht@cooper.edu>
2018-03-05 19:40:20 -0500

    Added all

--- SeeBerries.py
+++ SeeBerries.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import os
 import shutil
+import sys
 import time
 
 
@@ -25,7 +26,7 @@ def find_biggest_contour(image):
     cv2.drawContours(mask, contours, -1, 255, -1)
     return contours, mask, centers
 
-def detect_berries(image):
+def detect_berries(image, name):
     #image = cv2.imread(f)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # Blur image slightly
@@ -37,32 +38,32 @@ def detect_berries(image):
     min_red = np.array([0, 100, 80])
     max_red = np.array([10, 256, 256])
     image_red1 = cv2.inRange(image_blur_hsv, min_red, max_red)
-    #plt.imsave("./outputs/bs_outputs/" + os.path.basename(f).split(".")[0] + "_a_1.png", image_red1, cmap='gray')
+    plt.imsave("/home/krishna/Dropbox/Grover/outputs/" + os.path.basename(name).split(".")[0] + "_image_red1.png", image_red1, cmap='gray')
 
     # 170-180 hue
     min_red2 = np.array([170, 100, 80])
     max_red2 = np.array([175, 256, 256])
     image_red2 = cv2.inRange(image_blur_hsv, min_red2, max_red2)
-    #plt.imsave("./outputs/bs_outputs/" + os.path.basename(f).split(".")[0] + "_a_2.png", image_red2, cmap='gray')
+    plt.imsave("/home/krishna/Dropbox/Grover/outputs/" + os.path.basename(name).split(".")[0] + "_image_red2.png", image_red2, cmap='gray')
     image_red = image_red1 + image_red2
-    #plt.imsave("./outputs/bs_outputs/" + os.path.basename(f).split(".")[0] + "_b.png", image_red, cmap='gray')
+    plt.imsave("/home/krishna/Dropbox/Grover/outputs/" + os.path.basename(name).split(".")[0] + "_image_red.png", image_red, cmap='gray')
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
     image_red_closed = cv2.morphologyEx(image_red, cv2.MORPH_CLOSE, kernel)
     image_red_closed_then_opened = cv2.morphologyEx(image_red_closed, cv2.MORPH_OPEN, kernel)
-    #plt.imsave("./outputs/bs_outputs/" + os.path.basename(f).split(".")[0] + "_c.png", image_red_closed, cmap='gray')
-    #plt.imsave("./outputs/bs_outputs/" + os.path.basename(f).split(".")[0] + "_d.png", image_red_closed_then_opened, cmap='gray')
+    plt.imsave("/home/krishna/Dropbox/Grover/outputs/" + os.path.basename(name).split(".")[0] +  "_image_red_closed.png", image_red_closed, cmap='gray')
+    plt.imsave("/home/krishna/Dropbox/Grover/outputs/" + os.path.basename(name).split(".")[0] + "_image_red_closed_then_opened.png", image_red_closed_then_opened, cmap='gray')
 
 
     contours, red_mask, centers = find_biggest_contour(image_red_closed_then_opened)
 
-    #plt.imsave("./outputs/bs_outputs/" + os.path.basename(f).split(".")[0] + "_e.png", red_mask, cmap='gray')
+    plt.imsave("/home/krishna/Dropbox/Grover/outputs/" + os.path.basename(name).split(".")[0] +  "_red_mask.png", red_mask, cmap='gray')
     image_overlayed_mask = overlay_mask(red_mask, image)
-    #plt.imsave("./outputs/bs_outputs/" + os.path.basename(f).split(".")[0] + "_f.png", image_overlayed_mask)
+    plt.imsave("/home/krishna/Dropbox/Grover/outputs/" + os.path.basename(name).split(".")[0] + "_image_overlayed_mask.png", image_overlayed_mask)
 
     image_with_com = image.copy()
     for center in centers:
         cv2.circle(image_with_com, tuple(center), 10, (255, 255, 0), -1)
-    #plt.imsave("./outputs/bs_outputs/" + os.path.basename(f).split('.')[0] + "_g.png", image_with_com)
+    plt.imsave("/home/krishna/Dropbox/Grover/outputs/" + os.path.basename(name).split(".")[0] + "_image_with_com.png", image_with_com)
 
     image_with_ellipse = image.copy()
 
@@ -78,3 +79,10 @@ def stereo_vision(img1, img2, title):
     stereo_vision = cv2.StereoBM_create(numDisparities=16, blockSize=15)
     disparity=stereo_vision.compute(img1, img2)
     plt.imsave(title, disparity)
+
+
+if __name__=='__main__':
+    files = [os.path.join(sys.argv[1], f) for f in os.listdir(sys.argv[1]) if f.endswith('.jpeg') or f.endswith('.jpg')]
+    for f in files:
+        img = cv2.imread(f, cv2.COLOR_BGR2RGB)
+        plt.imsave('/home/krishna/Dropbox/Grover/outputs/' + f.replace('./', '').replace('jpg', 'png'), detect_berries(img, f))
